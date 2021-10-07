ed82fc0af1c5635ef874d381db7e80331caa2329
Hugo <HF77554@gmail.com>
2021-10-07 14:06:02 -0400

    REST routes have been created and GET testing has yielded the correct response

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,16 +2,30 @@ const express = require('express')
 const router = express.Router()
 
 
-/*
-REST rubric
 
-Getting all subcsribers
-Getting One subscriber
-Creating One subscriber
-Updating One subscriber
-Deleting One subscriber
+//REST rubric
+//Getting all subcsribers
+router.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
+//Getting One subscriber
+router.get('/:id', (req, res) =>{
+    res.send(req.params.id)
+})
+//Creating One subscriber
+router.post('/', (req, res) =>{
+    res.send('Hello World')
+})
+//Updating One subscriber
+router.patch('/:id', (req, res) =>{
+    
+})
+//Deleting One subscriber
+router.delete('/:id', (req, res) =>{
+    
+})
 
-*/
+
 
 
 module.exports = router
