ed70434ecd9293dc46285bff8df30aedcbc1e040
Hugo <HF77554@gmail.com>
2021-10-07 13:29:48 -0400

    created the subscriber route for the beginning of the REST format API

---
+++ routes/subscribers.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+
+/*
+REST rubric
+
+Getting all subcsribers
+Getting One subscriber
+Creating One subscriber
+Updating One subscriber
+Deleting One subscriber
+
+*/
+
+
+module.exports = router
