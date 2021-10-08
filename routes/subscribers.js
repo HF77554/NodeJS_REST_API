fce2d61cf96dce07f233d7f1e8989467526daca5
Hugo <HF77554@gmail.com>
2021-10-08 09:49:32 -0400

    added and tested the POST routing for user creation

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,20 +1,34 @@
 const express = require('express')
 const router = express.Router()
-
-
+const Subscriber = require('../models/subscriber')
 
 //REST rubric
 //Getting all subcsribers
-router.get('/', (req, res) =>{
-    res.send('Hello World!')
+router.get('/', async (req, res) =>{
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({message:err.message})
+    }
 })
 //Getting One subscriber
 router.get('/:id', (req, res) =>{
     res.send(req.params.id)
 })
 //Creating One subscriber
-router.post('/', (req, res) =>{
-    res.send('Hello World')
+router.post('/', async (req, res) =>{
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
 })
 //Updating One subscriber
 router.patch('/:id', (req, res) =>{
